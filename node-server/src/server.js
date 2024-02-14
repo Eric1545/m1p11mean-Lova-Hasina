bb0fbd70eac819f1879b0333b979fb672cf4b78a
hasina <hasinafitiavana2002@gmail.com>
2024-02-14 22:14:30 +0300

    upload file api

--- node-server/src/server.js
+++ node-server/src/server.js
@@ -1,19 +1,24 @@
 // src/app.js
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectDB = require('./database/database');
 const routes = require('./routes');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../uploads')));
 
 connectDB();
 
 app.use('/api', routes);
+
+// app.post('/upload', upload.single('file'),(req, res) => {
+//   res.json({ message: 'File uploaded successfully' });
+// })
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
