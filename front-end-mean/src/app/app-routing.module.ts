b7b344fc5fb3639c14fbe7c6cb282146c40df1fc
Eric1545 <100596011+Eric1545@users.noreply.github.com>
2024-02-14 20:00:48 +0300

    feat: integration template

--- front-end-mean/src/app/app-routing.module.ts
+++ front-end-mean/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {TemplateComponent} from "./template/template.component";
+import {ListeUtilisateurComponent} from "./utilisateur/liste-utilisateur/liste-utilisateur.component";
 
 const routes: Routes = [
+  {
+    path: 'teste',
+    component: TemplateComponent,
+    children: [
+      {
+        path: '',
+        component: ListeUtilisateurComponent
+      },
+    ]
+  },
   {path: '', redirectTo: '/liste-utilisateur', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent}
 ];
